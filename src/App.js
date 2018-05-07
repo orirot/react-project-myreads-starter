f75771ed69027b0e5484185bc87cb65019b063da
orirot <orirot6@gmail.com>
2018-05-08 00:13:17 +0300

    render books from backend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import './App.css'
+import React from 'react';
+import { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
+import './App.css';
 import ListBooks from "./ListBooks";
-import { Route } from 'react-router-dom';
+import SearchInBooks from "./SearchInBooks";
+import {Route} from 'react-router-dom';
 
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
+class BooksApp extends Component {
+    state = {
+        books: []
+    }
 
-  render() {
-    return (
-        <Route exact path ="/" render ={() => (
-          <ListBooks showSearch={this.state.showSearchPage}/>
-        )}/>
+    componentDidMount() {
+        BooksAPI.getAll()
+            .then((books) => {
 
-    )
-  }
+                this.setState(() => ({
+                    books
+                }));
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <Route exact path="/" render={() => (
+                    <ListBooks
+                        books = {this.state.books}
+                    />
+                )}/>
+                <Route path="/search" component={SearchInBooks}/>
+            </div>
+        )
+    }
 }
 
 export default BooksApp
